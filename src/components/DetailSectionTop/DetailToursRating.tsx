738c7419a39ad32698c4b7b9cb2aa664d4ae0d6c
NohWookJin <jinn98@kakao.com>
2023-12-29 09:59:13 +0900

    Feat: 디테일 페이지 마크업

---
+++ src/components/DetailSectionTop/DetailToursRating.tsx
@@ -0,0 +1,15 @@
+import { ReactComponent as StarIcon } from '../../assets/images/Star.svg';
+
+export default function DetailToursRating() {
+  return (
+    <div className="flex w-full items-center gap-1">
+      <StarIcon fill="yellow" />
+      <StarIcon fill="yellow" />
+      <StarIcon fill="yellow" />
+      <StarIcon fill="yellow" />
+      <StarIcon fill="#EDEDED" />
+
+      <p className="text-xs font-normal leading-4">(4)</p>
+    </div>
+  );
+}
