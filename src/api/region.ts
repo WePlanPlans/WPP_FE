f2560793eb03e543db685f2cce2670d46e3aebb1
Hojin <wkdghwls0623@gmail.com>
2024-01-01 22:52:50 +0900

    Chore: 지역 관련 API 함수명 수정

--- src/api/region.ts
+++ src/api/region.ts
@@ -3,13 +3,13 @@ import client from './client';
 // 지역 관련 API
 
 // 전체 지역 조회
-export const getTours = async (areaCode: number) => {
+export const getRegion = async (areaCode: number) => {
   const res = await client.get(`region?areaCode=${areaCode}`);
   return res;
 };
 
 // 인기 지역 조회
-export const getPopularTours = async () => {
+export const getPopularRegion = async () => {
   const res = await client.get(`region/popular`);
   return res;
 };
