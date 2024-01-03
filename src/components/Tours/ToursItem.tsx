a96bb130dab6ced1577c85799612d90e5b733898
sue <dldustn2040@gmail.com>
2024-01-04 01:44:22 +0900

    Design: 여행지 css 수정

--- src/components/Tours/ToursItem.tsx
+++ src/components/Tours/ToursItem.tsx
@@ -19,13 +19,13 @@ const ToursItem = ({ tour }: { tour: TourType }) => {
     <div
       className="cursor-pointer flex-col"
       onClick={() => navigate(`detail/${id}`)}>
-      <div>
+      <div className="relative">
         <img
-          className="rounded-1 h-[134px] max-h-[134px] w-[160px] rounded-[16px] object-cover"
+          className="rounded-1 h-[134px] max-h-[134px] w-[178px] rounded-[16px] object-cover"
           src={smallThumbnailUrl}
           alt="여행지 이미지"
         />
-        <div className="relative left-32 top-[-125px] z-10">
+        <div className="absolute right-[8px] top-[8px] z-10 w-[24px]">
           <HeartIcon
             size={24}
             color={liked ? '#ff2167' : '#ffffff'}
@@ -33,20 +33,28 @@ const ToursItem = ({ tour }: { tour: TourType }) => {
           />
         </div>
       </div>
-      <p className="headline1 ml-[1px] mt-[-14px] overflow-hidden text-ellipsis whitespace-nowrap leading-normal">
+      <p className="headline1 mt-2 overflow-hidden text-clip whitespace-nowrap px-[2px] leading-normal">
         {title}
       </p>
-      <div className="caption2 flex items-center leading-normal text-gray4">
+      <div className="caption1 ml-[2px] flex items-start leading-normal text-gray4">
         <div className="mr-[5px] flex items-center">
-          <StarIcon size={16} color="#FFEC3E" fill="#FFEC3E" />
+          <div className="mb-[2px]">
+            <StarIcon size={16} color="#FFEC3E" fill="#FFEC3E" />
+          </div>
           <div className="flex items-center pt-[1.5px]">
-            <span className="ml-1 mr-0.5 font-bold">{ratingAverage}</span>
-            <span>({reviewCount})</span>
+            <span className="ml-1 mr-0.5 font-bold">
+              {(Math.ceil(ratingAverage * 100) / 100).toFixed(1)}
+            </span>
+            <span>({reviewCount.toLocaleString()})</span>
           </div>
         </div>
         <div className="flex items-center">
-          <HeartIcon size={16} color="#FF2167" fill="#FF2167" />
-          <span className="ml-0.5 pt-[1.5px]">{likedCount}</span>
+          <div className="mb-[2px]">
+            <HeartIcon size={16} color="#FF2167" fill="#FF2167" />
+          </div>
+          <span className="ml-0.5 mt-[1.5px]">
+            {likedCount.toLocaleString()}
+          </span>
         </div>
       </div>
     </div>
