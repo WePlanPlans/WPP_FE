0ceb32dd10bb25f71144885ecb3c288b157a0e93
sue <dldustn2040@gmail.com>
2024-01-04 13:38:12 +0900

    Design: 여정생성 버튼추가

---
+++ src/components/Tours/CreateTripButton.tsx
@@ -0,0 +1,11 @@
+import { PlusIcon } from '@components/common/icons/Icons';
+
+const CreateTripButton = () => {
+  return (
+    <div className="sticky bottom-20 z-[100] ml-auto h-[51px] w-[51px] cursor-pointer">
+      <PlusIcon size={51} color="white" />
+    </div>
+  );
+};
+
+export default CreateTripButton;
