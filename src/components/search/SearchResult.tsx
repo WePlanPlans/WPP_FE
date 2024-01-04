8bf65ad86ea0ce18c323bcdbe19ab7a26909abe9
joanShim <joanjeongahshim@gmail.com>
2024-01-04 13:17:09 +0900

    Feat: 로딩 spinner 추가

--- src/components/search/SearchResult.tsx
+++ src/components/search/SearchResult.tsx
@@ -3,6 +3,7 @@ import { ResultCategory } from './ResultCategory';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import ToursCategoryItem from '@components/Tours/ToursCategoryItem';
 import { useEffect, useState } from 'react';
+import { Spinner } from '@components/common/spinner/Spinner';
 
 interface SearchResultProps {
   selectedRegion: string | null;
@@ -40,21 +41,29 @@ export const SearchResult = ({
           size: 10,
         }),
       initialPageParam: 0,
-      getNextPageParam: (_, allPages) => {
-        const nextPage = allPages.length + 1;
-        return nextPage;
+      getNextPageParam: (lastPage, allPages) => {
+        // 'last' 필드가 false이면, 다음 페이지가 존재함
+        if (!lastPage.data.last) {
+          // 다음 페이지 번호 반환
+          return allPages.length + 1;
+        }
+        // 그렇지 않으면, 더 이상 페이지가 없으므로 undefined 반환
+        return undefined;
       },
       enabled: !!searchWord,
     });
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Spinner />;
   }
   if (isError) {
     console.log('error fetching search result ');
   }
 
+  console.log('hasNextPage', hasNextPage);
+
   const searchResults = data?.pages.flatMap((page) => page.data.data.content);
+  // const searchOptions = data?
   console.log('searchResults', searchResults);
 
   return (
@@ -78,6 +87,7 @@ export const SearchResult = ({
           data={searchResults}
           category={selectedCategory}
           fetchNextPage={hasNextPage ? fetchNextPage : null}
+          hasNextPage={hasNextPage}
         />
       ) : (
         <div>검색결과가 없습니다.</div>
