4d9794a2af9b6dbfaed6b8749a83a46634d8643c
sue <dldustn2040@gmail.com>
2024-01-04 14:13:20 +0900

    Design: 메인페이지 css 수정

--- src/components/Tours/CreateTripButton.tsx
+++ src/components/Tours/CreateTripButton.tsx
@@ -2,7 +2,7 @@ import { PlusIcon } from '@components/common/icons/Icons';
 
 const CreateTripButton = () => {
   return (
-    <div className="sticky bottom-20 z-[100] ml-auto h-[51px] w-[51px] cursor-pointer">
+    <div className="sticky bottom-20 z-[100] ml-auto mr-[10px] h-[51px] w-[51px] cursor-pointer">
       <PlusIcon size={51} color="white" />
     </div>
   );
