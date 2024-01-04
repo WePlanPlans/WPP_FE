1d1e4e4ad163b0b36a07590df9953df2e3217ecf
Hojin <wkdghwls0623@gmail.com>
2024-01-04 11:18:04 +0900

    Refator: disabled 추가, 변수명 리펙토링

--- src/pages/signup/SignupSurvey.tsx
+++ src/pages/signup/SignupSurvey.tsx
@@ -8,10 +8,12 @@ const SignupSurvey = () => {
     SignupSurveyList.map(() => null),
   );
 
-  const handleOptionClick = (sectionIndex: number, optionIndex: number) => {
+  console.log(selectedOptions);
+
+  const handleOptionClick = (listIndex: number, optionIndex: number) => {
     setSelectedOptions(
       selectedOptions.map((selected, index) => {
-        if (index === sectionIndex) {
+        if (index === listIndex) {
           return optionIndex;
         }
         return selected;
@@ -21,7 +23,7 @@ const SignupSurvey = () => {
 
   return (
     <div className="flex h-[95vh] flex-col ">
-      <div className="relative flex flex-col items-start justify-start gap-3 p-4">
+      <div className="relative mb-[30px] flex flex-col items-start justify-start gap-3 p-4">
         <p className="flex-shrink-0 flex-grow-0 text-center text-2xl font-bold text-black">
           어떤 여행을 좋아하세요?
         </p>
@@ -30,23 +32,23 @@ const SignupSurvey = () => {
         </p>
       </div>
       <div className="scrollbar-hide flex-grow overflow-y-auto">
-        {SignupSurveyList.map((section, sectionIndex) => (
+        {SignupSurveyList.map((list, listIndex) => (
           <div
-            key={section.title}
+            key={list.title}
             className="mb-[32px] flex flex-col items-start justify-start gap-4 px-4 ">
             <div className="relative flex h-6 w-[335px] items-center justify-start gap-2 py-2 ">
               <p className="text-left text-base font-bold text-black">
-                {section.title}
+                {list.title}
               </p>
             </div>
             <div className="flex items-start justify-between gap-2 ">
-              {section.options.map((option, optionIndex) => (
+              {list.options.map((option, optionIndex) => (
                 <button
                   key={option.text}
-                  onClick={() => handleOptionClick(sectionIndex, optionIndex)}>
+                  onClick={() => handleOptionClick(listIndex, optionIndex)}>
                   <SignupSurveyOption
                     text={option.text}
-                    active={selectedOptions[sectionIndex] === optionIndex}
+                    active={selectedOptions[listIndex] === optionIndex}
                   />
                 </button>
               ))}
@@ -54,7 +56,9 @@ const SignupSurvey = () => {
           </div>
         ))}
       </div>
-      <Button onClick={() => {}}>완료</Button>
+      <Button isActive={!selectedOptions.includes(null)} onClick={() => {}}>
+        완료
+      </Button>
     </div>
   );
 };
