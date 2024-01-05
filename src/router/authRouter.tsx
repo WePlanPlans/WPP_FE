ac46fc74c64b862ca0fe6daae5bacaaaba17f69b
jisu Seo <jseo9732@gmail.com>
2024-01-05 21:02:15 +0900

    Refactor: 라우터 구조 리펙토링

--- src/router/authRouter.tsx
+++ src/router/authRouter.tsx
@@ -1,14 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
-import AuthLayout from './authLayout';
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from './mainLayout';
 import { Signup, SignupInfo } from '@pages/signup';
 import { Login } from '@pages/index';
 
 const AuthRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<AuthLayout />}>
+      <Route path="/" element={<MainLayout />}>
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signup" element={<SignupInfo />} />
+        <Route path="/signup/info" element={<SignupInfo />} />
         <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
--- src/router/mainLayout.tsx
+++ src/router/mainLayout.tsx
@@ -1,15 +1,21 @@
 import { Header } from '@components/common/header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Nav } from '@components/common/nav';
 
 const MainLayout = () => {
+  const location = useLocation();
+  const hideNavPaths = ['/signup', '/login'];
+  const showNav = !hideNavPaths.some((path) =>
+    location.pathname.includes(path),
+  );
+
   return (
-    <div className="mx-auto my-0 flex min-h-[100vh] max-w-[412px] flex-col bg-white ">
+    <div className="mx-auto h-screen max-w-[412px] bg-white">
       <Header />
-      <div className="px-[20px] py-0">
+      <div className="px-5">
         <Outlet />
       </div>
-      <Nav />
+      {showNav && <Nav />}
     </div>
   );
 };
