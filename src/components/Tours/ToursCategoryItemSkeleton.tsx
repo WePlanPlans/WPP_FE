293f0f98334f94e1560d9907d843770774b6541f
sue <dldustn2040@gmail.com>
2024-01-06 04:48:37 +0900

    Feat: 인기여행지 카테고리 스켈레톤 적용

---
+++ src/components/Tours/ToursCategoryItemSkeleton.tsx
@@ -0,0 +1,9 @@
+const ToursCategoryItemSkeleton = () => {
+  return (
+    <div className="flex animate-pulse items-center justify-center">
+      <div className="h-[40px] w-[58px] rounded-[30px] bg-gray-300 px-[16px] py-[7px]"></div>
+    </div>
+  );
+};
+
+export default ToursCategoryItemSkeleton;
