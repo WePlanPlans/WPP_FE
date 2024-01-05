8070150a83ef064ef86da200eb7fab82a0fe1601
jisu Seo <jseo9732@gmail.com>
2024-01-05 18:47:50 +0900

    Design: AuthLayout 수정

--- src/router/authLayout.tsx
+++ src/router/authLayout.tsx
@@ -1,11 +1,9 @@
-import { Header } from '@components/common/header';
 import { Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
   return (
-    <div className="mx-auto my-0 flex min-h-[100vh] max-w-[412px] flex-col bg-white ">
-      <Header />
-      <div className="px-[20px] py-0">
+    <div className="mx-auto h-screen max-w-[412px] bg-white">
+      <div className="px-5">
         <Outlet />
       </div>
     </div>
