a59b8c9e4cb4561456163834bb5d5ce8638948eb
NohWookJin <jinn98@kakao.com>
2024-01-06 10:54:12 +0900

    feat: 상세 관심 여행지 등록 및 삭제 기능 구현

--- src/components/DetailSectionTop/DetailToursInfo.tsx
+++ src/components/DetailSectionTop/DetailToursInfo.tsx
@@ -1,11 +1,52 @@
 import { HeartIcon } from '@components/common/icons/Icons';
+import { postLikedTours, deleteLikedTours } from '@api/tours';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 interface DetailToursInfoProps {
   infoData: tourDetail;
 }
 
 export default function DetailToursInfo({ infoData }: DetailToursInfoProps) {
-  const { title, liked, originalThumbnailUrl } = infoData;
+  const { title, liked, originalThumbnailUrl, id } = infoData;
+
+  const [isLiked, setIsLiked] = useState<boolean>(liked);
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const { mutate: likeMutate } = useMutation({
+    mutationFn: (id: number) => postLikedTours({ id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['details'] });
+      setIsLiked(true);
+    },
+    onError: () => console.log('error'),
+  });
+
+  const { mutate: unlikeMutate } = useMutation({
+    mutationFn: (id: number) => deleteLikedTours({ id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['details'] });
+      setIsLiked(false);
+    },
+    onError: () => console.log('error'),
+  });
+
+  const onClickLikeButton = () => {
+    const token = localStorage.getItem('accessToken');
+    if (!token) {
+      // 비로그인 알람창 처리 필요
+      navigate('/signin');
+    } else {
+      if (liked === false) {
+        likeMutate(id);
+      } else {
+        unlikeMutate(id);
+      }
+    }
+  };
 
   return (
     <>
@@ -20,15 +61,14 @@ export default function DetailToursInfo({ infoData }: DetailToursInfoProps) {
         <h1 className="font-['Pretendard'] text-2xl font-bold text-black ">
           {title}
         </h1>
-        {liked ? (
-          <div className="top-75 h-[24px] w-[24px] cursor-pointer">
-            <HeartIcon fill="#FF2167" color="none" />
-          </div>
-        ) : (
-          <div className="top-75 h-[24px] w-[24px] cursor-pointer">
-            <HeartIcon fill="#D7D7D7" color="none" />
-          </div>
-        )}
+        <div className="top-75 h-[24px] w-[24px] cursor-pointer">
+          <HeartIcon
+            // 최종 = props liked 기반, 현재 임시 처리(isLiked)
+            fill={isLiked ? '#FF2167' : '#D7D7D7'}
+            color="none"
+            onClick={onClickLikeButton}
+          />
+        </div>
       </div>
     </>
   );
