9dda1407e85ce3a2156befd9ae482cb9e7877c9a
jisu Seo <jseo9732@gmail.com>
2024-01-06 19:20:14 +0900

    Style: 로그인 실패 경고 문구 수정

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -87,7 +87,7 @@ const Login = () => {
           {!errors.email && !errors.password && isLoginFailed && (
             <>
               <ErrorMessage>
-                아이디(로그인 전용 아이디) 또는 비밀번호를 잘못 입력했습니다.
+                이메일 또는 비밀번호를 잘못 입력했습니다.
               </ErrorMessage>
               <ErrorMessage>입력하신 내용을 다시 확인해주세요.</ErrorMessage>
             </>
