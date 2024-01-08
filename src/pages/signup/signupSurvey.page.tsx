0e707cc100cfeca87f50790dbfa4e12dee4910a7
jisu Seo <jseo9732@gmail.com>
2024-01-09 04:05:49 +0900

    Design: 회원가입 취향 조사 마크업

--- src/pages/signup/signupSurvey.page.tsx
+++ src/pages/signup/signupSurvey.page.tsx
@@ -1,16 +1,16 @@
 import { AuthTitle } from '@components/Auth';
 import { BackBox } from '@components/common';
-import { Link } from 'react-router-dom';
+import AuthSurvey from '@components/Auth/AuthSurvey/AuthSurvey';
 
 const SignupSurvey = () => {
   return (
-    <div className="flex h-[95vh] flex-col">
+    <div className="flex h-full flex-col">
       <BackBox isShowSkip />
       <AuthTitle
         title={'어떤 여행을 좋아하세요?'}
         subTitle="여행 취향을 골라주세요."
       />
-      <Link to="/signup/info">다음</Link>
+      <AuthSurvey />
     </div>
   );
 };
