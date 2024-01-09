255ecba37bdf33e3f9e2afef1137b6fffdc0eef1
jisu Seo <jseo9732@gmail.com>
2024-01-09 20:09:51 +0900

    Refactor: 카카오 로그인 임시

--- src/api/authClient.ts
+++ src/api/authClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const authClient = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL,
+  baseURL: `${import.meta.env.VITE_SERVER_URL}api/`,
   headers: {
     'Content-Type': 'application/json',
   },
