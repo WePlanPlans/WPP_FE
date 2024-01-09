956c9b6e19f240d0834b313abf13b78299ed632a
NohWookJin <jinn98@kakao.com>
2024-01-09 13:10:00 +0900

    Feat: 관심여행지 기능 구현

---
+++ src/components/Wish/WishCategory.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import WishCategoryItem from './WishCategoryItem';
+
+interface WishCategoryProps {
+  onCategoryClick: (contentTypeId: number | null) => void;
+}
+
+const WishCategory: React.FC<WishCategoryProps> = ({ onCategoryClick }) => {
+  const [selectedCategory, setSelectedCategory] = useState<string>('전체');
+
+  const categories = [
+    { code: null, name: '전체' },
+    { code: 12, name: '관광지' },
+    { code: 32, name: '숙소' },
+    { code: 39, name: '식당' },
+  ];
+
+  const handleSelectCategory = (name: string) => {
+    setSelectedCategory(name);
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className="no-scrollbar mb-6 mt-3 flex w-[100%] overflow-scroll overflow-y-hidden bg-white">
+      {categories.map((category) => {
+        return (
+          <WishCategoryItem
+            key={category.code}
+            category={category}
+            onCategoryClick={onCategoryClick}
+            isSelected={category.name === selectedCategory}
+            onSelect={handleSelectCategory}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default WishCategory;
