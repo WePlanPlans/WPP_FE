21bde9dc51755e5ed1382fbd6093bc0e3841f2f1
NohWookJin <jinn98@kakao.com>
2024-01-10 15:19:02 +0900

    refactor: 비로그인 유저 알럿 처리

--- src/pages/wishList/wishList.page.tsx
+++ src/pages/wishList/wishList.page.tsx
@@ -1,28 +1,11 @@
 import Wish from '@components/Wish/Wish';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const WishList = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = window.localStorage.getItem('accessToken');
-    if (token) {
-      setIsLoggedIn(true);
-    } else {
-      navigate('/signin');
-    }
-  }, []);
-
-  if (isLoggedIn) {
-    return (
-      <>
-        <Wish />
-      </>
-    );
-  }
+  return (
+    <>
+      <Wish />
+    </>
+  );
 };
 
 export default WishList;
