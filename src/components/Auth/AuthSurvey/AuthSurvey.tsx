29af634af0ee9d178593768694936ba895e3e439
jisu Seo <jseo9732@gmail.com>
2024-01-12 06:09:22 +0900

    Style: console.log 삭제

--- src/components/Auth/AuthSurvey/AuthSurvey.tsx
+++ src/components/Auth/AuthSurvey/AuthSurvey.tsx
@@ -51,7 +51,6 @@ const AuthSurvey = ({ path }: Props) => {
       alert('오류가 발생했습니다. 다시 시도해주세요');
     }
   };
-  console.log(isDirty);
 
   return (
     <form className="mb-8" onSubmit={handleSubmit(onSaveSubmit)}>
