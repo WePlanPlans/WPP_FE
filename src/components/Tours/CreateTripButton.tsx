39d894476d3e0cee00a3f6eec6a2f293386f96af
joanShim <joanjeongahshim@gmail.com>
2024-01-11 23:35:38 +0900

    Feat: 메인 FAB에서 경로 이동설정

--- src/components/Tours/CreateTripButton.tsx
+++ src/components/Tours/CreateTripButton.tsx
@@ -3,12 +3,13 @@ import { useNavigate } from 'react-router-dom';
 const CreateTripButton = () => {
   const navigate = useNavigate();
 
-  const onClick = () => {
-    navigate('/create');
-  };
   return (
     <div className="sticky bottom-20 z-[100] ml-auto mr-2 flex h-12 w-[119px] items-center justify-center gap-2 rounded-[30px] bg-[#28d8ff] p-2 shadow-[2px_2px_5px_0_rgba(0,0,0,0.2)]">
-      <button className="headline1 pt-[2px] text-white" onClick={onClick}>
+      <button
+        className="headline1 pt-[2px] text-white"
+        onClick={() => {
+          navigate('/create');
+        }}>
         여행 계획하기
       </button>
     </div>
