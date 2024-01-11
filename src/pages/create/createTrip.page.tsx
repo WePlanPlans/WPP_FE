875b5b075674d945839e504d6e0c462ef70664cc
joanShim <joanjeongahshim@gmail.com>
2024-01-11 23:52:34 +0900

    Fix: 인풋 삭제 아이콘 수정

--- src/pages/create/createTrip.page.tsx
+++ src/pages/create/createTrip.page.tsx
@@ -1,10 +1,15 @@
 import BackHeader from '@components/common/header/BackHeader';
 import { useState } from 'react';
 import { SelectDate } from '../../components/createTrip/SelectDate';
+import { CloseIcon } from '@components/common/icons/Icons';
 
 export const CreateTrip = () => {
   const [inputValue, setInputValue] = useState('나의 여정');
   const [showSelectDate, setShowSelectDate] = useState(false);
+
+  const clearInput = () => {
+    setInputValue('');
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -25,13 +30,23 @@ export const CreateTrip = () => {
     <div>
       <BackHeader />
       <div className="title1 mt-2 pb-5">여행 생성하기</div>
-      <input
-        type="text"
-        className="mb-2 h-[54px] w-full rounded-lg border border-zinc-300 p-4 text-black placeholder-gray4"
-        placeholder="나의 여정 이름을 입력하세요"
-        value={inputValue}
-        onChange={handleChange}
-      />
+      <div className="relative mb-3 flex items-center">
+        <input
+          type="text"
+          className="mb-2 h-[54px] w-full rounded-lg border border-zinc-300 p-4 text-black placeholder-gray4 focus:outline-none"
+          placeholder="나의 여정 이름을 입력하세요"
+          value={inputValue}
+          onChange={handleChange}
+          autoFocus
+        />
+        {inputValue && (
+          <div
+            className="absolute right-2 top-1/2 z-10 -translate-y-1/2 transform cursor-pointer rounded-full bg-gray3"
+            onClick={clearInput}>
+            <CloseIcon />
+          </div>
+        )}
+      </div>
       <input
         type="button"
         className="mb-2 h-[54px] w-full cursor-pointer rounded-lg border border-zinc-300 p-4  text-left text-black placeholder-gray4"
