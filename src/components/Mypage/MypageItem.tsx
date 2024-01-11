7ae650e5a822095a128b29f5f3d593fcb19b88fb
jisu Seo <jseo9732@gmail.com>
2024-01-12 06:09:06 +0900

    Feat: 로그인 여부 판단 후 마이페이지 이동

--- src/components/Mypage/MypageItem.tsx
+++ src/components/Mypage/MypageItem.tsx
@@ -1,5 +1,6 @@
 import { RightIcon } from '@components/common/icons/Icons';
-import { Link } from 'react-router-dom';
+import { getItem } from '@utils/localStorageFun';
+import { useNavigate } from 'react-router-dom';
 
 interface Props {
   link: string;
@@ -7,13 +8,25 @@ interface Props {
 }
 
 const MypageItem = ({ link, children }: Props) => {
+  const navigate = useNavigate();
+
+  const onMypageClick = () => {
+    if (getItem('accessToken')) {
+      navigate(link);
+    } else {
+      if (confirm('로그인이 필요해요. 로그인하시겠어요?')) {
+        navigate('/login');
+      }
+    }
+  };
+
   return (
-    <Link
-      to={link}
+    <button
+      onClick={onMypageClick}
       className="flex h-14 w-full items-center justify-between py-2">
       <span className="body3 text-black">{children}</span>
       <RightIcon />
-    </Link>
+    </button>
   );
 };
 
