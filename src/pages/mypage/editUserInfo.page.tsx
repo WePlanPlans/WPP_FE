5fcbd614a747313791916b6ed3f11614f2c8ffca
jisu Seo <jseo9732@gmail.com>
2024-01-11 10:49:26 +0900

    Design: 비밀번호 변경 버튼 생성

--- src/pages/mypage/editUserInfo.page.tsx
+++ src/pages/mypage/editUserInfo.page.tsx
@@ -1,5 +1,6 @@
 import DeleteMemberButton from '@components/Mypage/DeleteMemberButton';
 import { BackBox } from '@components/common';
+import { Link } from 'react-router-dom';
 
 const EditUserInfo = () => {
   return (
@@ -7,8 +8,10 @@ const EditUserInfo = () => {
       <BackBox showBack showSave saveHandler={() => {}}>
         회원정보 수정
       </BackBox>
-      <div className="flex flex-col">
-        <button className="body4 text-gray4">비밀번호 변경</button>
+      <div className="flex flex-col items-center">
+        <Link to="password" className="body4 text-gray4">
+          비밀번호 변경
+        </Link>
         <DeleteMemberButton />
       </div>
     </>
