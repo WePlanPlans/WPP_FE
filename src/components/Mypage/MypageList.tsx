ac835e73ddda38212a4731754368f192df410504
NohWookJin <jinn98@kakao.com>
2024-01-14 18:04:05 +0900

    feat: 마이페이지 나의여정 URL 설정

--- src/components/Mypage/MypageList.tsx
+++ src/components/Mypage/MypageList.tsx
@@ -3,7 +3,7 @@ import MypageItem from './MypageItem';
 const MypageList = () => {
   return (
     <div>
-      <MypageItem link="/mytour">나의 여정</MypageItem>
+      <MypageItem link="/mytrip">나의 여정</MypageItem>
       <MypageItem link="/wishlist">나의 관심 여행지</MypageItem>
       <MypageItem link="/myPageReview">나의 리뷰</MypageItem>
       <MypageItem link="/mypage/survey">나의 여행 취향 설정</MypageItem>
