2d7dbad486a17781911f524c52e2915758911af4
joanShim <joanjeongahshim@gmail.com>
2024-01-14 17:55:29 +0900

    Feat: 여행 생성 api 연동

--- src/pages/create/createTrip.page.tsx
+++ src/pages/create/createTrip.page.tsx
@@ -14,12 +14,34 @@ import { ko } from 'date-fns/locale';
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { SelectDate } from '../../components/createTrip/SelectDate';
+import { postTrips } from '@api/trips';
 
 export const CreateTrip = () => {
   const [title, setTitle] = useState('');
   const [numOfMembers, setNumOfMembers] = useState(2);
   const [showSelectDate, setShowSelectDate] = useState(false);
   const [showSelectDestination, setShowSelectDestination] = useState(false);
+
+  const handleSubmit = async () => {
+    try {
+      const tripRequestData = {
+        tripName: title || '나의 N번째 여정', // title이 빈 문자열일 경우 기본값 설정
+        numberOfPeople: numOfMembers,
+        startDate: tripDate.startDate
+          ? tripDate.startDate.toISOString().split('T')[0]
+          : null,
+        endDate: tripDate.endDate
+          ? tripDate.endDate.toISOString().split('T')[0]
+          : null,
+      };
+
+      const response = await postTrips(tripRequestData);
+      console.log('전송 완료: ', response);
+      // 리코일 초기화
+    } catch (error) {
+      console.error('전송 실패: ', error);
+    }
+  };
 
   const handleIncrease = () => {
     setNumOfMembers((prevNum) => prevNum + 1);
@@ -32,11 +54,8 @@ export const CreateTrip = () => {
   const tripDate = useRecoilValue(tripDateState);
   const formattedTripDate =
     tripDate.startDate && tripDate.endDate
-      ? `${format(tripDate.startDate, 'MM.dd', { locale: ko })} - ${format(
-          tripDate.endDate,
-          'MM.dd',
-          { locale: ko },
-        )}`
+      ? `${format(tripDate.startDate, 'MM.dd', { locale: ko })} - 
+      ${format(tripDate.endDate, 'MM.dd', { locale: ko })}`
       : '여행 날짜(선택)';
 
   if (showSelectDate) {
@@ -119,7 +138,7 @@ export const CreateTrip = () => {
       </InputField>
 
       <div className="mt-auto">
-        <ButtonPrimary onClick={() => {}}>완료</ButtonPrimary>
+        <ButtonPrimary onClick={handleSubmit}>완료</ButtonPrimary>
       </div>
     </div>
   );
