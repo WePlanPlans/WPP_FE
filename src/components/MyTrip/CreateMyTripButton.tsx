adaee407488ebbc6f01761926bcaae2a23812f97
NohWookJin <jinn98@kakao.com>
2024-01-16 00:53:51 +0900

    feat: 여정 생성 버튼

--- src/components/MyTrip/CreateMyTripButton.tsx
+++ src/components/MyTrip/CreateMyTripButton.tsx
@@ -1,6 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+
 const CreateMyTripButton = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="sticky bottom-20 z-[100] ml-auto mr-2 flex h-12 w-[119px] items-center justify-center gap-2 rounded-[30px] bg-[#28d8ff] p-2 shadow-[2px_2px_5px_0_rgba(0,0,0,0.2)]">
+    <div
+      onClick={() => navigate('/create')}
+      className="sticky bottom-20 z-[100] ml-auto mr-2 flex h-12 w-[119px] items-center justify-center gap-2 rounded-[30px] bg-[#28d8ff] p-2 shadow-[2px_2px_5px_0_rgba(0,0,0,0.2)]">
       <button className="headline1 pt-[2px] text-white">여행 계획하기</button>
     </div>
   );
