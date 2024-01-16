3cc3bfa4902861023d6ad4f60e898d4a122a2df1
joanShim <joanjeongahshim@gmail.com>
2024-01-16 18:47:00 +0900

    Refactor: 버튼 영역 수정

--- src/components/Tours/CreateTripButton.tsx
+++ src/components/Tours/CreateTripButton.tsx
@@ -26,19 +26,17 @@ const CreateTripButton = () => {
         title={'로그인'}
         message={
           <>
-            나의 여정 조회를 위해 로그인이 필요합니다.
+            로그인이 필요한 기능입니다.
             <br />
             로그인 하시겠습니까?
           </>
         }
         onConfirm={handleConfirm}>
-        <div className="sticky bottom-20 z-[100] ml-auto mr-2 flex h-12 w-[119px] items-center justify-center gap-2 rounded-[30px] bg-[#28d8ff] p-2 shadow-[2px_2px_5px_0_rgba(0,0,0,0.2)]">
-          <button
-            className="headline1 pt-[2px] text-white"
-            onClick={handleCreateTrip}>
-            여행 계획하기
-          </button>
-        </div>
+        <button
+          className="sticky bottom-20 z-[100] ml-auto mr-2 flex h-12 w-[119px] items-center justify-center gap-2 rounded-[30px] bg-[#28d8ff] p-2 shadow-[2px_2px_5px_0_rgba(0,0,0,0.2)]"
+          onClick={handleCreateTrip}>
+          <div className="headline1 pt-[2px] text-white">여행 계획하기</div>
+        </button>
       </Alert>
     </>
   );
