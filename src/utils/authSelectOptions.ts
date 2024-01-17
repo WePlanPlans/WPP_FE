b0c51b26b9c7e136360e99bef381d062115b22ed
jisu Seo <jseo9732@gmail.com>
2024-01-17 17:00:47 +0900

    Refactor: 드랍다운 임시 구현

---
+++ src/utils/authSelectOptions.ts
@@ -0,0 +1,15 @@
+export const genderArr: SelectOption[] = [
+  { id: '', value: '성별을 선택해주세요.' },
+  { id: 'FEMALE', value: '여' },
+  { id: 'MALE', value: '남' },
+  { id: 'NON_BINARY', value: '기타' },
+];
+
+export const ageArr: SelectOption[] = [
+  { id: '', value: '연령대를 설정해주세요.' },
+  { id: 'TEENAGER', value: '10대' },
+  { id: 'TWENTIES', value: '20대' },
+  { id: 'THIRTIES', value: '30대' },
+  { id: 'FOURTIES', value: '40대' },
+  { id: 'ABOVE_FIFTIES', value: '50대 이상' },
+];
