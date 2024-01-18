5083a61582f7df92ef6e02ebc4d3cee680a4b68b
NohWookJin <jinn98@kakao.com>
2024-01-18 14:29:24 +0900

    feat: 추가버튼 클릭시 검색 라우터 이동

--- src/components/Trip/LikedToursList.tsx
+++ src/components/Trip/LikedToursList.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
 import LikedToursListCategory from './LikedToursLists/LikedToursListCategory';
@@ -13,7 +13,10 @@ export const LikedToursList = () => {
   const [selectedContentTypeId, setSelectedContentTypeId] = useState<
     null | number
   >(null);
+
+  const navigate = useNavigate();
   const params = useParams();
+
   const [selectedTripId, _] = useState(Number(params.id));
 
   const { fetchNextPage, hasNextPage, data, isLoading, error } =
@@ -40,13 +43,13 @@ export const LikedToursList = () => {
       },
     });
 
-  const handleCategoryClick = (contentTypeId: number | null) => {
-    setSelectedContentTypeId(contentTypeId);
-  };
-
   if (error) {
     return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
   }
+
+  const handleCategoryClick = (contentTypeId: number | null) => {
+    setSelectedContentTypeId(contentTypeId);
+  };
 
   return (
     <div className="realtive">
@@ -73,9 +76,11 @@ export const LikedToursList = () => {
         </div>
       )}
 
-      {/* 검색 라우터 이동 */}
+      {/* 우리의 관심 여행지 추가 버튼 => 검색 라우터 이동 */}
       <div className="sticky bottom-5 left-1/2 z-[100] inline-flex h-10 w-[168px] -translate-x-1/2 transform items-center justify-center gap-1.5 rounded-3xl border border-solid border-cyan-400 bg-white px-3.5">
-        <button className="flex items-center justify-between">
+        <button
+          onClick={() => navigate('add')}
+          className="flex items-center justify-between">
           <NewIcon />
           <span className="ml-[5px] mt-[0.5px] text-sm font-semibold text-cyan-400">
             우리의 관심목록 추가
