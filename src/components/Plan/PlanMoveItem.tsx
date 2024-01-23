81648e81ae13122cead79bd80bc69304048dcaa0
Hojin <wkdghwls0623@gmail.com>
2024-01-23 11:54:14 +0900

    Fix: 바텀시트 반응형 적용(양옆짤림 해결)

--- src/components/Plan/PlanMoveItem.tsx
+++ src/components/Plan/PlanMoveItem.tsx
@@ -85,17 +85,17 @@ const PlanMoveItem: React.FC<PlanMoveItemProps> = ({
 
         <Dialog.Portal>
           <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 z-10 bg-black opacity-70" />
-          <Dialog.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[97%] z-10 h-[238px] w-[412px] translate-x-[-50%] translate-y-[-90%]  rounded-tl-[16px] rounded-tr-[16px] bg-white pb-[34px] focus:outline-none">
+          <Dialog.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[97%] z-10 h-[238px] w-[100vw] max-w-[412px] translate-x-[-50%] translate-y-[-90%] rounded-tl-[16px] rounded-tr-[16px] bg-white pb-[34px] focus:outline-none">
             <Dialog.Description className="text-mauve11 mt-[10px] text-[15px] leading-normal">
               <div className="relative flex flex-col items-start justify-start">
                 <div className="absolute left-0 top-0 flex flex-col items-start justify-start">
-                  <div className="relative flex h-12 w-[375px] flex-shrink-0 flex-grow-0 items-center justify-start gap-2 rounded-tl-2xl rounded-tr-2xl bg-white px-5 pb-1 pt-2">
-                    <p className="flex-shrink-0 flex-grow-0 text-left text-[15px] font-bold text-[#1e1e1e]">
+                  <div className="relative flex h-12 w-[95vw] max-w-[395px] flex-shrink-0 flex-grow-0 items-center justify-start gap-2 rounded-tl-2xl rounded-tr-2xl bg-white px-5 pb-1 pt-2">
+                    <p className="w-full flex-shrink-0 flex-grow-0 text-left text-[15px] font-bold text-[#1e1e1e]">
                       날짜 이동
                     </p>
                   </div>
                   <div className="flex h-[156px] flex-shrink-0 flex-grow-0 flex-col items-start justify-start overflow-y-scroll">
-                    <div className="flex h-[52px] w-[375px] flex-shrink-0 flex-grow-0 flex-col justify-start gap-4  bg-white px-5 py-3 ">
+                    <div className="flex h-[52px] w-[95vw] max-w-[395px] flex-shrink-0 flex-grow-0 flex-col justify-start gap-4  bg-white px-5 py-3 ">
                       {day.map((day, index) => (
                         <Dialog.Close asChild key={day}>
                           <button
