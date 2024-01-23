3914deb3ad2bca663fc4a6a3faaeb3ce64ac675b
어승준 <eoseungjun7@naver.com>
2024-01-23 15:07:20 +0900

    Fix: 리뷰 작성, 헤더 스타일 개선

--- src/components/Review/ReviewButton.tsx
+++ src/components/Review/ReviewButton.tsx
@@ -1,7 +1,7 @@
 import { ButtonPrimary } from '@components/common/button/Button';
-import { useState, useEffect } from 'react';
-import { contentState, keywordsState, ratingState } from '@recoil/review';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { ratingState } from '@recoil/review';
+import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 
 interface ButtonProps {
   onClick: () => void;
@@ -9,28 +9,8 @@ interface ButtonProps {
 
 const ReviewButton = (props: ButtonProps) => {
   const { onClick } = props;
-  const [content] = useRecoilState(contentState);
-  const keywords = useRecoilValue(keywordsState);
   const rating = useRecoilValue(ratingState);
-  const [isContentValid, setIsContentValid] = useState(false);
-  const [isKeywordsValid, setIsKeywordsValid] = useState(false);
   const [isRatingValid, setIsRatingValid] = useState(false);
-
-  useEffect(() => {
-    if (content.length >= 10) {
-      setIsContentValid(true);
-    } else if (content.length < 10) {
-      setIsContentValid(false);
-    }
-  }, [content]);
-
-  useEffect(() => {
-    if (keywords.length > 0) {
-      setIsKeywordsValid(true);
-    } else if (keywords.length <= 0) {
-      setIsKeywordsValid(false);
-    }
-  }, [keywords]);
 
   useEffect(() => {
     if (rating > 0) {
@@ -41,28 +21,19 @@ const ReviewButton = (props: ButtonProps) => {
   }, [rating]);
 
   return (
-    <div className="pb-4">
-      {isRatingValid === false && (
-        <div className="mb-2 flex items-center justify-center text-xs text-[#FF0F00]">
-          별점을 입력해주세요
-        </div>
-      )}
-
-      {isRatingValid === true &&
+    <div className=" bottom-0  mt-auto flex w-full items-center justify-center pb-4">
+      {/* {isRatingValid === true &&
         isContentValid === false &&
         isKeywordsValid === false && (
           <div className="mb-2 flex items-center justify-center text-xs text-[#FF0F00]">
             키워드를 선택하거나 텍스트를 10자 이상 입력해주세요
           </div>
-        )}
+        )} */}
 
       <ButtonPrimary
         onClick={onClick}
         className="flex h-[56px] items-center justify-center"
-        disabled={
-          isRatingValid === false ||
-          (isContentValid === false && isKeywordsValid === false)
-        }>
+        disabled={isRatingValid === false}>
         완료
       </ButtonPrimary>
     </div>
